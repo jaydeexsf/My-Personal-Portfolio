d126eef2365ea7f52287a3cada59d6fae2cff66a
johannes <jaydeexsf@gmail.com>
2024-04-02 12:43:11 +0200

    12:43

--- js/app.js
+++ js/app.js
@@ -1,27 +1,25 @@
-// Nav hamburgerburger selections
+// Nav sideburger selections
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
-// Scrolling to top selection
 const scrollUp = document.querySelector("#scroll-up");
 
-// Select nav links
 const navLink = document.querySelectorAll(".nav-link");
 
-// Hamburger menu function
+// side menu function
 burger.addEventListener("click", () => {
   ul.classList.toggle("show");
 });
 
-// Close hamburger menu when a link is clicked
+// Close side menu when a link is clicked
 navLink.forEach((link) =>
   link.addEventListener("click", () => {
     ul.classList.remove("show");
   })
 );
 
-// scroll to top functionality
+// scroll to the top of the page
 scrollUp.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
